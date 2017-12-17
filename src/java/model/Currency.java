c6c48fe13e7e10119815e47a002ebe02929f06b5
Alex <>
2017-12-17 18:41:33 +0100

    final

--- src/java/model/Currency.java
+++ src/java/model/Currency.java
@@ -3,14 +3,11 @@ package model;
 
 import java.io.Serializable;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Currency implements Serializable{
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
     private String name;
     private float rate;
     
